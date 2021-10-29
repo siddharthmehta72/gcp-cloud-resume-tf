9faf95489b93cb69c41453d7512f8d950ad0c260
Sidd <you@example.com>
2021-10-29 21:54:08 +0530

    updated source codes for cloud storage

--- cloudstorage.tf
+++ cloudstorage.tf
@@ -3,5 +3,9 @@ module "static-assets_cloud-storage-static-website" {
   version = "0.6.0"
   # insert the 2 required variables here
   project = "firm-catalyst-327616"
-  website_domain_name = "sidd-cloud-resume-static"
+  website_domain_name = "siddharthmehta.in"
+  website_storage_class = "REGIONAL"
+  website_location = "asia-south2"
+  force_destroy_access_logs_bucket = true
+  force_destroy_website = true
 }
--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "google" {
     project = "	firm-catalyst-327616"
     region = "asia-south2"
+    GOOGLE_APPLICATION_CREDENTIALS= ".\terraform-creds.json"
 }
